d920c7d4b669b3250156ee1258224227c533754d
Carlos Manuel Peguero Gomez <cmpeguerog@gmail.com>
2021-05-05 01:35:42 -0400

    Add data re-import check.

--- scripts/insert.sh
+++ scripts/insert.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
+
+if [ -f "/.exit" ]; then
+  echo "Data already imported into MongoDB"
+  exit 0
+elif [ -f "./.exist" ]; then
+  echo "Data already imported into MongoDB"
+  exit 0
+fi
+
 for file in /tmp/scripts/*.json; do
   echo "Inserting file " $file
   mongoimport --host mongo --db gateway --collection peripherals --type json --file $file --jsonArray
 done
+
+echo "data imported" >> .exist
 
